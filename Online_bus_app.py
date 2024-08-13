3c5b2259f9f7a83fd6b6633e3ed653670d748a2a
AswinBalaji88 <167520681+AswinBalaji88@users.noreply.github.com>
2024-08-14 01:50:51 +0530

    Update Online_bus_app.py

--- Online_bus_app.py
+++ Online_bus_app.py
@@ -6,7 +6,7 @@ import pandas as pd
 def get_connection():
     return mysql.connector.connect(
         host="localhost",
-        port=3306
+        port=3306,
         user="root",
         password="12345678",
         database="redbus",
